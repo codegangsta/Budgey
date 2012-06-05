cd3297cc21205538bafce22542db827c571a505d
Jeremy Saenz <jeremy@leedostudios.com>
2012-06-04 20:40:54 -0700

    Initial commit

---
+++ BudgeyTests/BudgeyTests.h
@@ -0,0 +1,13 @@
+//
+//  BudgeyTests.h
+//  BudgeyTests
+//
+//  Created by Jeremy Saenz on 06/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BudgeyTests : SenTestCase
+
+@end
---
+++ BudgeyTests/BudgeyTests.m
@@ -0,0 +1,32 @@
+//
+//  BudgeyTests.m
+//  BudgeyTests
+//
+//  Created by Jeremy Saenz on 06/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BudgeyTests.h"
+
+@implementation BudgeyTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BudgeyTests");
+}
+
+@end
